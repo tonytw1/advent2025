3231f71e4afd31e3bdd195d2dc5c6593082402ca
Tony McCrae <tony@eelpieconsulting.co.uk>
2025-12-08 12:41:54 +0000

    Day 1 part 2

--- day1.test.ts
+++ day1.test.ts
@@ -1,6 +1,11 @@
-import { day1 } from './day1';
+import { part1, part2 } from './day1';
 
 test('day1 part 1', () => {
-  expect(day1('day1-example.txt')).toBe(3);
-  expect(day1('day1.txt')).toBe(969);
+  expect(part1('day1-example.txt')).toBe(3);
+  expect(part1('day1.txt')).toBe(969);
+});
+
+test('day1 part 2', () => {
+  expect(part2('day1-example.txt')).toBe(6);
+  expect(part2('day1.txt')).toBe(5887);
 });
--- day1.ts
+++ day1.ts
@@ -1,66 +1,94 @@
+import { error } from 'console';
 import * as fs from 'fs';
 
-export function day1(filename: string): number {
+const dialSteps = 100;
+
+export function part1(filename: string): number {
     const moves = parseInput(filename);
 
-    const dialSteps = 100;
+    var p = 50
     var zeros = 0
-    return part1();
-
-    function part1(): number {
-        var p = 50
-        for (const move of moves) {
-            p = dialPositionAfterMove(p, move);
-            if (p === 0) {
-                zeros += 1;
-            }
+    for (const move of moves) {
+        p = dialPositionAfterMove(p, move);
+        if (p === 0) {
+            zeros += 1;
         }
-        return zeros;
     }
-
-    function part2(): number {
-        var p = 50
-        for (const move of moves) {
-            // Final dial postion is the same as part 1
-            p = dialPositionAfterMove(p, move);
-
-            // Evaulate how many times this crosses zero with out brute forcing it
-            // Probably a function of the delta
-            if (p === 0) {
-                zeros += 1;
-            }
-        }
-        return zeros;
-    }
-
-    function dialPositionAfterMove(p: number, move: number) {
-        if (move > 0)
-            p += move;
-        else {
-            // map left moves into match right moves for easier wrap around
-            p += (dialSteps - Math.abs(move));
-        }
-        // Normalise
-        p = p % dialSteps;
-        return p;
-    }
-
-
-    function parseInput(filename: string) {
-        const input = fs.readFileSync(filename, 'utf8');
-        const moves = [];
-        const lines = input.split(/\r?\n/);
-        for (let i = 0; i < lines.length; i++) {
-            const line = lines[i];
-            if (line.startsWith("L")) {
-                const newLocal = line.slice(1);
-                moves.push((Number(newLocal) * -1));
-            } else {
-                const newLocal = line.slice(1);
-                moves.push(Number(newLocal));
-            }
-        }
-        return moves;
-    }
-
+    return zeros;
 }
+
+export function part2(filename: string): number {
+    const moves = parseInput(filename);
+
+    var p = 50
+    var zeros = 0
+    for (const move of moves) {
+        // Final dial postion is the same as part 1
+
+        // Evaulate how many times this crosses zero with out brute forcing it
+        // Probably a function of the delta
+        // Number of times through 0 is probably a proxy for know many full rotations have been made 
+        // with an allowance for how close you started to 0.
+        zeros += zeroCrossingsByDelta(p, move);
+        p = dialPositionAfterMove(p, move);
+    }
+    return zeros;
+
+    function zeroCrossingsByBruteForce(start: number, move: number): number {
+        // Used to cross check the delta based approach
+        var zeros = 0;
+        var p = start;
+        for (let i = 0; i < Math.abs(move); i++) {
+            p = dialPositionAfterMove(p, Math.sign(move));
+            if (p === 0) {
+                zeros += 1;
+            }
+        }
+        return zeros;
+    }
+
+    function zeroCrossingsByDelta(start: number, move: number): number {
+        const delta = Math.abs(move);
+        if (move > 0) {
+            const distanceFromZero = start;
+            const t = delta + distanceFromZero;
+            return Math.floor(t / dialSteps);
+
+        } else {
+            const distanceFromZero = dialSteps - start
+            var t = delta + distanceFromZero;
+            if (start == 0) {
+                t -= dialSteps;
+            }
+            return Math.floor(t / dialSteps);
+        }
+        return 0;
+    }
+}
+
+function dialPositionAfterMove(p: number, move: number) {
+    p += move;
+    // Normalise
+    p = p % dialSteps;
+    if (p < 0) {
+        p += dialSteps;
+    }
+    return p;
+}
+
+function parseInput(filename: string) {
+    const input = fs.readFileSync(filename, 'utf8');
+    const moves = [];
+    const lines = input.split(/\r?\n/);
+    for (const line of lines) {
+        if (line.startsWith("L")) {
+            const newLocal = line.slice(1);
+            moves.push((Number(newLocal) * -1));
+        } else {
+            const newLocal = line.slice(1);
+            moves.push(Number(newLocal));
+        }
+    }
+    return moves;
+}
+
