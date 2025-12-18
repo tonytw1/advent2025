55cf89ca565a96fea2ffaf9032842917628f3fd0
Tony McCrae <tony@eelpieconsulting.co.uk>
2025-12-18 09:12:41 +0000

    day 2 part 1; small enough to just brute force

--- day2.ts
+++ day2.ts
@@ -4,31 +4,24 @@ const dialSteps = 100;
 
 export function part1(filename: string): number {
     const ranges = parseInput(filename);
-    
+
     var invalidTotal = 0;
-    var r  = 1;  
     for (const range of ranges) {
-        console
-        const start = range[0]; 
+        const start = range[0];
         const end = range[1];
 
         for (let n = start; n <= end; n++) {
             const asString = n.toString();
             if (asString.length % 2 == 0) {
-               const middle = asString.length / 2;
-               const left = asString.substring(0, middle);
-               const right = asString.substring(middle);
-
-               //console.log("  Left: " + left + " Right: " + right);
-               if (left === right) {
-                invalidTotal += n;
-               }
+                const middle = asString.length / 2;
+                const left = asString.substring(0, middle);
+                const right = asString.substring(middle);
+                if (left === right) {
+                    invalidTotal += n;
+                }
             }
         }
-        r += 1;
     }
-
-
     return invalidTotal;
 }
 
@@ -37,8 +30,6 @@ function parseInput(filename: string) {
     const ranges: number[][] = [];
     const rangeInputs = input.split(",");
     for (const rangeInput of rangeInputs) {
-        console.log("Parsing range: " + rangeInput);
-
         const split = rangeInput.split("-");
         const start = Number(split[0]);
         const end = Number(split[1]);
