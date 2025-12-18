54e399c4917f3fae8e0e3aa32352bb341bed4f19
Tony McCrae <tony@eelpieconsulting.co.uk>
2025-12-18 15:38:00 +0000

    Day 2 part 2

--- day2.test.ts
+++ day2.test.ts
@@ -1,6 +1,13 @@
-import { part1 } from './day2';
+import { part1, part2 } from './day2';
 
 test('day2 part 1', () => {
   expect(part1('day2-example.txt')).toBe(1227775554);
   expect(part1('day2.txt')).toBe(56660955519);
 });
+
+test('day2 part 2', () => {
+  expect(part2('day2-example.txt')).toBe(4174379265);
+  expect(part2('day2.txt')).toBe(79183223243);
+});
+
+
--- day2.ts
+++ day2.ts
@@ -23,6 +23,50 @@ export function part1(filename: string): number {
     return invalidTotal;
 }
 
+export function part2(filename: string): number {
+    const ranges = parseInput(filename);
+
+    function isInvalid(n: number): boolean {
+        const asString = n.toString();
+
+        // Step through each allowed sequence length
+        for (let l = 1; l < asString.length; l++) {
+            if (asString.length % l !== 0) {
+                // Discard lengths which won't fit evenly
+                continue;
+            }
+
+            const sequence = asString.substring(0, l);
+            for (let s = l; s < asString.length; s += l) {
+                const substring = asString.substring(s, s + l);
+                if (substring != sequence) {
+                    break
+                }
+                if (s + l == asString.length) {
+                    return true;
+                }
+            }
+
+        }
+        return false;
+    }
+
+    var invalidTotal = 0;
+    for (const range of ranges) {
+        const start = range[0];
+        const end = range[1];
+        for (let n = start; n <= end; n++) {
+            if (isInvalid(n)) {
+                invalidTotal += n;
+            }
+        }
+
+    }
+
+
+    return invalidTotal;
+}
+
 function parseInput(filename: string) {
     const input = fs.readFileSync(filename, 'utf8');
     const ranges: number[][] = [];
