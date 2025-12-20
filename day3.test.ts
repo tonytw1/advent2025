5339d41181a6a042a2c02ebfccaadbc00c942abd
Tony McCrae <tony@eelpieconsulting.co.uk>
2025-12-20 18:25:30 +0000

    Day 3 part 2

--- day3.test.ts
+++ day3.test.ts
@@ -1,7 +1,14 @@
-import { part1 } from './day3';
+import { part1, part2 } from './day3';
 
 test('day3 part 1', () => {
   expect(part1('day3-example.txt')).toBe(357);
   expect(part1('day3.txt')).toBe(17166);
 });
+
+test('day3 part 2', () => {
+  expect(part2('day3-example.txt')).toBe(3121910778619);
+  expect(part2('day3.txt')).toBe(169077317650774);
+});
+
+
 
