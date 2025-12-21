cb491f6337f1073b389dc590cc989a1fd372b845
Tony McCrae <tony@eelpieconsulting.co.uk>
2025-12-21 19:07:42 +0000

    Day 5 part 2

--- day5.test.ts
+++ day5.test.ts
@@ -1,6 +1,12 @@
-import { part1 } from './day5';
+import { part1, part2 } from './day5';
 
 test('day5 part 1', () => {
   expect(part1('day5-example.txt')).toBe(3);
   expect(part1('day5.txt')).toBe(623);
+});
+
+test('day5 part 2', () => {
+  expect(part2('day5-example.txt')).toBe(14);
+  // 325034395180875 too low
+  expect(part2('day5.txt')).toBe(353507173555373);
 });
--- day5.ts
+++ day5.ts
@@ -1,3 +1,4 @@
+import { time } from 'console';
 import * as fs from 'fs';
 
 export function part1(filename: string): number {
@@ -12,12 +13,51 @@ export function part1(filename: string): number {
         // Foreach ingredient brute force the ranges looking for a fit
         for (const freshRange of freshRanges) {
             if (ingredient >= freshRange[0] && ingredient <= freshRange[1]) {
-                freshCount ++
+                freshCount++
                 break
             }
         }
     }
     return freshCount
+}
+
+export function part2(filename: string): number {
+    // The answer is the sum of the width of the ranges
+    // But because the ranges over lap we will want to merge overlaps first
+    const freshRanges = parseRanges(filename)
+
+    const rangesByStart = freshRanges.sort((a, b) => 
+        a[0] - b[0]
+    )
+
+    // Walk the ranges...
+    // Keep tcurrent range in scope.
+    // If the next range overlaps then expand the current range up to the next ranges end.
+    // If the next range does not overlap then append the current range to merged and make the next range the current one
+    // If a current range is left hanging at the end of the iteration then added it merged.
+    const mergedRanges = []
+    var currentRange: number[] = rangesByStart[0]
+    for (let i = 1; i < rangesByStart.length; i++) {
+        const range = rangesByStart[i]
+        const overlaps = range[0] <= currentRange[1]
+        if (overlaps) {
+            // Only if this range is really wider than the current one
+            if (range[1] > currentRange[1]) {
+                currentRange[1] = range[1]
+            }
+        } else {
+            mergedRanges.push(currentRange)
+            currentRange = range
+        }
+    }
+    mergedRanges.push(currentRange)
+
+    var total = 0
+    for (const range of mergedRanges) {
+        const rangeLength = (range[1] - range[0]) + 1
+        total += rangeLength
+    }
+    return total
 }
 
 function parseRanges(filename: string): number[][] {
@@ -39,7 +79,7 @@ function parseIngedients(filename: string): number[] {
     const ingredients: number[] = []
     const input = fs.readFileSync(filename, 'utf8');
     const lines = input.split(/\r?\n/);
-    var         isIngedient = false
+    var isIngedient = false
 
     for (const line of lines) {
         if (isIngedient) {
