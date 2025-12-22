575827fefac262cb4886194739e453399a27d78a
Tony McCrae <tony@eelpieconsulting.co.uk>
2025-12-22 20:09:13 +0000

    Day 6 part 2. Requires a refactor in the input parser to preserve the whitespace inside the parsed cells

--- day6.test.ts
+++ day6.test.ts
@@ -1,6 +1,11 @@
-import { part1 } from './day6';
+import { part1, part2 } from './day6';
 
 test('day6 part 1', () => {
   expect(part1('day6-example.txt')).toBe(4277556);
   expect(part1('day6.txt')).toBe(4878670269096);
+});
+
+test('day6 part 2', () => {
+  expect(part2('day6-example.txt')).toBe(3263827);
+  expect(part2('day6.txt')).toBe(8674740488592);
 });
--- day6.ts
+++ day6.ts
@@ -2,19 +2,56 @@
 import * as fs from 'fs';
 
 export function part1(filename: string): number {
-    const input = parseInput(filename)
+    const columns = parseInput(filename)
 
     var total = 0
     // Foreach column calculate the result
-    for (let c = 0; c < input[0].length; c++) {
+    for (const column of columns) {
         const numbers: number[] = []
         // Extract the numbes which are in normal left-to-right format
-        for (let r = 0; r < input.length - 1; r++) {
-            numbers.push(Number.parseInt(input[r][c]))
+        for (let r = 0; r < column.length - 1; r++) {
+            numbers.push(Number.parseInt(column[r].trim()))
         }
         // and the operator
-        const operator = input[input.length - 1][c]
+        const operator = column[column.length - 1].trim()
+        total += valueOfColumn(operator, numbers)
+    }
 
+    return total
+}
+
+export function part2(filename: string): number {
+    const columns = parseInput(filename)
+
+    var total = 0
+    // Foreach column calculate the result
+    for (const column of columns) {
+        // Extract the numbes which are column descending left to right format.
+        // Iterate across the vertical slots in the column.
+        // We need the width of the larges row todo this
+        var width = 0
+        for (let r = 0; r < column.length - 1; r++) {
+            const cell = column[r]
+            if (cell.length > width) {
+                width = cell.length
+            }
+        }
+
+        const numbers: number[] = []
+
+        // Scan the vertical slots to build the numbers
+        for (let w = 0; w < width; w++) {
+            var number = ""
+            for (let r = 0; r < column.length - 1; r++) {
+                const cell = column[r]
+                if (cell.length > w) {
+                    const c = cell.charAt(w)
+                    number = number + c
+                }
+            }
+            numbers.push(Number.parseInt(number))
+        }
+        const operator = column[column.length - 1].trim()
         total += valueOfColumn(operator, numbers)
     }
 
@@ -39,32 +76,49 @@ function valueOfColumn(operator: string, numbers: number[]) {
 
 function parseInput(filename: string) {
     // Read each line spliting by white space
-    // Parse into an a array of array of strings
+    // Parse into an a array of columns of rows
+    const columns: string[][] = []
 
     const input = fs.readFileSync(filename, 'utf8');
     const lines = input.split(/\r?\n/);
 
+    // Part 2 requires the white space within columns to be preserved!
+    // Therefore we need to scan for the further most right seperator which slicing off columns
 
-    const rows: string[][] = []
+    // The max width is required to deal with the termination of the last column
+    var maxLineWidth = -1
     for (const line of lines) {
-        const cells: string[] = [];
-        var buffer = ""
+        if (line.length > maxLineWidth) {
+            maxLineWidth = line.length
+        }
+    }
 
-        for (let i = 0; i < line.length; i++) {
-            const c = line.charAt(i)
-            if (c == " ") {
-                if (buffer.length > 0) {
-                    cells.push(buffer)
-                    buffer = ""
-                }
-            } else {
-                buffer = buffer + c
+    // Moving column slicing window until we reach max width of the lines
+    var cut = 0;
+    var newCut = 0;
+    while (cut < maxLineWidth) {
+        // Check each line for the max index of the separator
+        // Use this to determine the right most cut for this column
+        for (const line of lines) {
+            const remainingLine = line.slice(cut, maxLineWidth);
+            var indexOfNextColumnBreakOnThisLine = remainingLine.indexOf(" ");
+            if (indexOfNextColumnBreakOnThisLine < 0) {
+                indexOfNextColumnBreakOnThisLine = remainingLine.length
+            }
+            var s = indexOfNextColumnBreakOnThisLine + cut
+            if (s > newCut) {
+                newCut = s
             }
         }
-        if (buffer.length > 0) {
-            cells.push(buffer)
+
+        // Foreach row, slice off the column
+        const column: string[] = []
+        for (const line of lines) {
+            column.push(line.slice(cut, newCut))
         }
-        rows.push(cells)
+
+        columns.push(column)
+        cut = newCut + 1;
     }
-    return rows
+    return columns
 }
