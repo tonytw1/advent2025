5a31a5a707c38b4d339d1ab964c323506d4e9027
Tony McCrae <tony@eelpieconsulting.co.uk>
2026-01-01 21:12:54 +0000

    Day 7 part 1

---
+++ day7.test.ts
@@ -0,0 +1,5 @@
+import { part1 } from './day7';
+
+test('day7 part 1', () => {
+  expect(part1('day7-example.txt')).toBe(21);
+});
---
+++ day7.ts
@@ -0,0 +1,56 @@
+import * as fs from 'fs';
+
+export function part1(filename: string): number {
+    const [s, splitters] = parseInput(filename)
+
+    var beams = new Set<number>()
+    beams.add(s)
+
+    var totalSplits = 0
+    for (const line of splitters) {
+        // Compose the next set of beams based in this line's interaction with the current beams
+        console.log(beams)
+        const nextBeams = new Set<number>()
+        for (const beam of beams) {
+            if (line.has(beam)) {
+                // Split
+                nextBeams.add(beam - 1)
+                nextBeams.add(beam + 1)
+                totalSplits++
+            } else {
+                // This beam drops through
+                nextBeams.add(beam)
+            }
+        }
+        beams = nextBeams
+    }
+
+    return totalSplits
+}
+
+function parseInput(filename: string): [number, Set<number>[]] {
+    // Parse the input.
+    // Return the index of the start from the first row
+    // and a list of sets of indexes of the splitters
+    const input = fs.readFileSync(filename, 'utf8');
+    const lines = input.split(/\r?\n/);
+
+    const firstLine = lines[0]
+    const s = firstLine.indexOf('S')
+
+    const splitters: Set<number>[] = []
+    for (let i = 1; i < lines.length; i++) {
+        const line = lines[i]
+        const lineSplitters = new Set<number>()
+        for (let j = 0; j < line.length; j++) {
+            if (line.charAt(j) == '^') {
+                lineSplitters.add(j)
+            }
+        }
+        if (lineSplitters.size > 0) {
+            splitters.push(lineSplitters)
+        }
+    }
+
+    return [s, splitters]
+}
